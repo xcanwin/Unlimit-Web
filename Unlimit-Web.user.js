cba3f7adc519e4c917f24d828cdbae7a2a3f04cd
xcanwin <xcanwin@gmail.com>
2023-11-26 06:43:03 +0800

    修复初始化自动破解列表失效的bug

--- Unlimit-Web.user.js
+++ Unlimit-Web.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Unlimit-Web
 // @description  解除网页限制: 恢复文本的选中和复制, 过滤文本小尾巴, 恢复右键菜单. Remove webpage restrictions: restore the selection and copy of text, clear the text tail, and restore the right-click menu.
-// @version      6.0
+// @version      7.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/Unlimit-Web/
 // @supportURL   https://github.com/xcanwin/Unlimit-Web/
-// @updateURL    https://raw.githubusercontent.com/xcanwin/Unlimit-Web/main/Unlimit-Web.user.js
-// @downloadURL  https://raw.githubusercontent.com/xcanwin/Unlimit-Web/main/Unlimit-Web.user.js
+// @updateURL    https://greasyfork.org/scripts/400515-unlimit-web/code/Unlimit-Web.user.js
+// @downloadURL  https://greasyfork.org/scripts/400515-unlimit-web/code/Unlimit-Web.user.js
 // @icon         data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" stroke-width="2" fill="none" stroke="currentColor"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
 // @license      GPL-2.0-only
 // @match        *://blog.csdn.net/*
@@ -44,6 +44,9 @@
 
     const $ = (Selector, el) => (el || document).querySelector(Selector);
     const $$ = (Selector, el) => (el || document).querySelectorAll(Selector);
+
+    const symbol = "❌✔️";
+    let mc = []
 
     const sv = (key, value = "") => {
         GM_setValue(key, value);
@@ -123,6 +126,7 @@
         }
     };
 
+    /*加入自动破解列表*/
     const switchAuto = () => {
         let autolist = JSON.parse(gv("ul_autolist", "[]"));
         const domain = getdomain();
@@ -135,15 +139,19 @@
         rmc();
     };
 
+    /*查看自动破解列表*/
     const showAuto = () => {
         prompt("自动破解列表", gv("ul_autolist", "[]"));
     };
 
+    /*初始化自动破解列表*/
     const initAutoList = () => {
         const defaultal = ["blog.csdn.net","www.bilibili.com","360doc.com","guofeng.yuedu.163.com","kuwo.cn","chuangshi.qq.com","read.qidian.com","dafrok.github.io","shushan.zhangyue.net","aqistudy.cn","xuexila.com","51test.net","laokaoya.com","utaten.com","book.qq.com","doc.mbalib.com","oh100.com","51test.net","cspengbo.com","diyifanwen.com","ahsrst.cn","kt250.com"];
-        if (!gv("ul_autolist", "[]")) {
+        //为空或者为[]时，说明首次运行，进行初始化
+        if (gv("ul_autolist", "[]") === "[]") {
             sv("ul_autolist", JSON.stringify(defaultal));
         }
+        //解析移除时，进行初始化
         try {
             JSON.parse(gv("ul_autolist", "[]"));
         } catch (e) {
@@ -151,19 +159,19 @@
         }
     };
 
+    /*取消注册菜单*/
     const unrmc = () => {
         mc.forEach(x => GM_unregisterMenuCommand(x));
     };
 
+    /*注册菜单*/
     const rmc = () => {
         unrmc();
         let isauto;
-        initAutoList();
         const autolist = JSON.parse(gv("ul_autolist", "[]"));
         const domain = getdomain();
         if (autolist.includes(domain)) {
             isauto = 1;
-            unLimit();
         } else {
             isauto = 0;
         }
@@ -176,8 +184,16 @@
         return (new URL(location.href)).hostname;
     };
 
-    let mc = []
-    const symbol = "❌✔️";
-    rmc();
+    const main = () => {
+        initAutoList();
+        rmc();
+        const autolist = JSON.parse(gv("ul_autolist", "[]"));
+        const domain = getdomain();
+        if (autolist.includes(domain)) {
+            unLimit();
+        }
+    };
+
+    main();
 
 })();
